e152094765fbe8bbdec095d2f15a735829d703a7
valeskaYcp <valeska.condori@tecsup.edu.pe>
2024-11-25 11:52:15 -0500

    Creacion de InfoScreen

--- app/src/main/java/com/example/widgets/InformacionScreen.kt
+++ app/src/main/java/com/example/widgets/InformacionScreen.kt
@@ -1,2 +1,38 @@
 package com.example.widgets
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class InformacionScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InfoScreen()
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InfoScreen() {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text("Información de la App") })
+    }) {
+        Column(modifier = Modifier.padding(72.dp)) {
+            Text(
+                "Esta es una aplicación para mostrar información sobre cultivos",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
